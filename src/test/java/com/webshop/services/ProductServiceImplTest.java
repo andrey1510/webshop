04a58bb5dd2cabec4ba94476c284276b75ddc7b4
andrey1510 <andrey.ivn15@gmail.com>
2025-04-02 23:46:15 +0300

    refactoring

--- src/test/java/com/webshop/services/ProductServiceImplTest.java
+++ src/test/java/com/webshop/services/ProductServiceImplTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.codec.multipart.FilePart;
 import reactor.core.publisher.Flux;
